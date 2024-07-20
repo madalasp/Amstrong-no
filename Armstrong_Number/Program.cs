dfd48dcca41c0470cd0b6dc15529f9bd29226cbd
madalasp <dearsibaprasad@gmail.com>
2024-07-20 20:53:21 +0530

    Amstrong number

---
+++ Armstrong_Number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armstrong_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int no, temp, rem, sum=0;
+            Console.WriteLine("Enter a Number:");
+            no=int.Parse(Console.ReadLine());
+            temp = no;
+            while(no > 0)
+            {
+                rem = no % 10;
+                sum = sum + (rem * rem * rem);
+                no = no / 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine("it is Armstrong Number");
+            }
+            else
+            {
+                Console.WriteLine("it is not Armstrong Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
